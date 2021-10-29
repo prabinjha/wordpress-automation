132f23d290c839bb0419414ddb3ceffc6b4371c0
prabinjha <79081919+prabinjha@users.noreply.github.com>
2021-10-30 03:12:15 +0545

    initial commit

---
+++ wpautorunner.sh
@@ -0,0 +1,82 @@
+#/bin/sh
+
+sudo apt-get update -y
+#sudo apt-get upgrade -y
+sudo apt-get install apache2 apache2-utils -y
+sudo systemctl enable apache2
+sudo systemctl start apache2
+sudo ufw allow in "Apache"
+
+#mysql user data
+install_dir="/var/www/html"
+#Creating Random WP Database Credenitals
+db_name="wp`date +%s`"
+db_user=$db_name
+db_password=`date |md5sum |cut -c '1-12'`
+sleep 1
+mysqlrootpass=`date |md5sum |cut -c '1-12'`
+sleep 1
+
+sudo apt-get install mysql-client mysql-server -y
+apt-get install php libapache2-mod-php php-mysql php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip -y
+sudo systemctl restart apache2
+wget -c http://wordpress.org/latest.tar.gz
+tar -xzvf latest.tar.gz
+sudo mv wordpress/* /var/www/html/
+sudo chown -R www-data:www-data /var/www/html/
+sudo chmod -R 755 /var/www/html/
+#### Start mysql and set root password
+rm /var/www/html/index.html
+systemctl enable mysql
+systemctl start mysql
+
+/usr/bin/mysql -e "USE mysql;"
+/usr/bin/mysql -e "UPDATE user SET Password=PASSWORD($mysqlrootpass) WHERE user='root';"
+/usr/bin/mysql -e "FLUSH PRIVILEGES;"
+touch /root/.my.cnf
+chmod 640 /root/.my.cnf
+echo "[client]">>/root/.my.cnf
+echo "user=root">>/root/.my.cnf
+echo "password="$mysqlrootpass>>/root/.my.cnf
+
+systemctl restart apache2
+
+#### Create WP-config and set DB credentials
+/bin/mv $install_dir/wp-config-sample.php $install_dir/wp-config.php
+/bin/sed -i "s/database_name_here/$db_name/g" $install_dir/wp-config.php
+/bin/sed -i "s/username_here/$db_user/g" $install_dir/wp-config.php
+/bin/sed -i "s/password_here/$db_password/g" $install_dir/wp-config.php
+
+cat << EOF >> $install_dir/wp-config.php
+define('FS_METHOD', 'direct');
+EOF
+
+cat << EOF >> $install_dir/.htaccess
+# BEGIN WordPress
+<IfModule mod_rewrite.c>
+RewriteEngine On
+RewriteBase /
+RewriteRule ^index.php$ – [L]
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteRule . /index.php [L]
+</IfModule>
+# END WordPress
+EOF
+
+chown www-data: $install_dir -R
+
+##### Set WP Salts
+grep -A50 'table_prefix' $install_dir/wp-config.php > /tmp/wp-tmp-config
+/bin/sed -i '/**#@/,/$p/d' $install_dir/wp-config.php
+/usr/bin/lynx --dump -width 200 https://api.wordpress.org/secret-key/1.1/salt/ >> $install_dir/wp-config.php
+/bin/cat /tmp/wp-tmp-config >> $install_dir/wp-config.php && rm /tmp/wp-tmp-config -f
+/usr/bin/mysql -u root -e "CREATE DATABASE $db_name"
+/usr/bin/mysql -u root -e "CREATE USER '$db_name'@'localhost' IDENTIFIED WITH mysql_native_password BY '$db_password';"
+/usr/bin/mysql -u root -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'localhost';"
+ 
+######Display generated passwords to log file.
+echo "Database Name: " $db_name
+echo "Database User: " $db_user
+echo "Database Password: " $db_password
+echo "Mysql root password: " $mysqlrootpass
